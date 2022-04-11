adc5174e75c0d9f0e129f2e225cb96ff616d4323
dovmf <dovmf@218.233.81.192>
2022-04-11 20:35:32 +0900

    Init calender project

---
+++ src/honux/calender/calender.java
@@ -0,0 +1,11 @@
+package honux.calender;
+
+public class calender {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello calender");
+
+	}
+
+}
