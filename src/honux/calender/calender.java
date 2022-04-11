80c1f12d1adab71d87e95d1ab5714bc03e82eafb
dovmf <dovmf@218.233.81.192>
2022-04-11 20:57:52 +0900

    calendar stage1

--- src/honux/calender/calender.java
+++ src/honux/calender/calender.java
@@ -4,7 +4,12 @@ public class calender {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello calender");
+		System.out.println("Hello calender ");
+		System.out.println("월 화 수 목 금 토 일");
+		System.out.println("----------------");
+		System.out.println(" 1 2 3  4 5  6 7");
+		
+		
 
 	}
 
